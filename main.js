f644a07e5649c0a5059bba9307a75e20a9d3bbc1
amart26 <58822386+amart26@users.noreply.github.com>
2025-07-25 00:16:04 -0400

    Update game mechanics: Power ups

--- main.js
+++ main.js
@@ -1,27 +1,72 @@
+
+class PowerUp 
+{
+     constructor(name, cost, effect, amountPurchased) 
+     {
+        this.name = name; 
+        this.cost = cost; 
+        this.effect = effect;
+        this.amountPurchased = amountPurchased;
+     }
+
+     onPowerUpClick() 
+     { 
+       
+        if(currentGooCount >= this.cost)
+        {
+                this.amountPurchased++;
+                currentGooCount -= this.cost;
+                gooCounterTxt.textContent = currentGooCount;
+                gooPerSecond += this.effect;
+                gooPerSecondTxt.textContent = gooPerSecond;
+                powerUp1AmountTxt.textContent = this.amountPurchased;
+                console.log(currentGooCount);
+        }
+        else 
+        {
+                console.log("Error");
+        }
+     }
+}
+
 //getting text elements
 let gooCounterTxt = document.getElementById("goo-counter-span");
 let gooPerSecondTxt = document.getElementById("goo-per-second-count-span")
 
 //getting images
 const gooImg = document.getElementById("goo-img");
-const powerUpOne = document.getElementById("power-up-1");
-const powerUpTwo = document.getElementById("power-up-2");
-const powerUpThree = document.getElementById("power-up-3");
+const powerUpOneImg = document.getElementById("power-up-1");
+const powerUpTwoImg = document.getElementById("power-up-2");
+const powerUpThreeImg = document.getElementById("power-up-3");
 
 //global variables
-let currentGooCount = 0;
+let currentGooCount =0;
 let gooPerSecond = 100; 
-let time = 1000;
+const time = 1000;
+
+const powerUpOne = new PowerUp("x1", 10, 1, 0);
+const powerUpTwo = new PowerUp("x5", 50, 5, 0);
+const powerUpThree = new PowerUp("x5", 100, 10, 0);
 
 //initializations
 gooPerSecondTxt.textContent = gooPerSecond;
+
 setInterval(AddPassiveGoo, time);
+
 
 //event listeners
 gooImg.addEventListener("click", onGooClick);
+powerUpOneImg.addEventListener("click", () => powerUpOne.onPowerUpClick());
+powerUpTwoImg.addEventListener("click", () => powerUpTwo.onPowerUpClick());
+powerUpThreeImg.addEventListener("click", () => powerUpThree.onPowerUpClick());
+
+
+
+/* Fixing
 powerUpOne.addEventListener("click", onPowerUpClick);
-powerUpTwo.addEventListener("click", onPowerUpClick);
-powerUpThree.addEventListener("click", onPowerUpClick);
+powerUpTwo.addEventListener("click", onPowerUpTwoClick);
+powerUpThree.addEventListener("click", onPowerUpThreeClick);
+*/
 
 //functions
  function onGooClick() 
@@ -35,23 +80,45 @@ powerUpThree.addEventListener("click", onPowerUpClick);
 function AddPassiveGoo()
 {   
         currentGooCount += gooPerSecond; 
-        gooCounterTxt.textContent = currentGooCount;
-    
+        gooCounterTxt.textContent= currentGooCount;
+     
 }
 
-function onPowerUpClick()
+/* Three functions to control power ups ***Optimizing at
+a later date
+function onPowerUpOneClick()
 {
-    let addOneGPS = 1;
-    let addFiveGPS = 5;
-    let addTenGPS = 10; 
+    let oneGPS = 1;
+    gooPerSecond += oneGPS;
 
+    currentGooCount += gooPerSecond; 
 
+    gooPerSecondTxt.textContent = gooPerSecond; 
+    console.log(gooPerSecond);
+}
+function onPowerUpTwoClick()
+{
+    let fiveGPS = 5;
+    gooPerSecond += fiveGPS;
+
+    currentGooCount += gooPerSecond; 
+
+    gooPerSecondTxt.textContent = gooPerSecond; 
+    console.log(gooPerSecond);
 }
 
+function onPowerUpThreeClick()
+{
+     let tenGPS = 10; 
+     gooPerSecond += tenGPS;
 
-//debugging
-console.log(currentGooCount); 
-console.log(gooPerSecond);
+     currentGooCount += gooPerSecond; 
+
+     gooPerSecondTxt.textContent = gooPerSecond; 
+     console.log(gooPerSecond);
+}
+*/
+
 
 
 
